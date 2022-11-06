154c77873c3ba5a3cffa741dd61c11e74bce2cef
NighthawkTheProto <Dennisbirousecondary@gmail.com>
2022-11-06 14:56:07 +0000

    Added dark/light mode switching logic and trigger
    Added dark/light mode switching logic and trigger to Index page, need to make it switch whole HTML though or find a way to make Body cover whole page in any aspect

--- myScript.js
+++ myScript.js
@@ -1,10 +1,5 @@
+//document.getElementById("demo").style.cursor = "default"; this will stop the mouse from changing to clickable
 
-document.getElementById("demo").style.cursor = "pointer";
-
-function myFunction(){
-document.getElementById("demo").innerHTML = "This text is from JavaScript!";
-document.getElementById("demo").style.cursor = "default"; //this will stop the mouse from changing to clickable
-}
 
 
 //Side navigation
@@ -35,3 +30,30 @@ function onClickNavpg3(){
     localton.href = "About.html"
 }
 //can use getelementbyid to make a download button
+
+//Need way to change whole HTML too
+function lightDarkBodysort(){
+var curId1 = document.getElementById('mainbodyLight') //Add in light mode ID
+var curId2 = document.getElementById('mainbodyDark') //Add in dark mode ID
+
+if(curId1 != null){
+LighttoDark();
+}
+
+else if (curId2 != null){
+DarktoLight();
+}
+else{
+    console.error("None of described IDs found, FUNCT: lightDarkBodySort(), LINES: 1, 2... 4,8 (Relative to function). Contact Devs")
+}
+
+function LighttoDark(){
+var curId = document.getElementById('mainbodyLight'); //Checks for Light mode ID
+curId.setAttribute('id','mainbodyDark'); //sets dark mode ID
+}
+
+function DarktoLight(){
+var curId = document.getElementById('mainbodyDark'); //checs for Dark mode ID
+curId.setAttribute('id','mainbodyLight'); //sets light mode ID
+}
+}
